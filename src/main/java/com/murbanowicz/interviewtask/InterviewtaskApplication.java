bf14f89fe29e565a25f820b6440308929ac22374
Max Urbanowicz <m.urbanowicz1@gmail.com>
2024-02-19 19:51:44 +0100

    initial commit - project setup

---
+++ src/main/java/com/murbanowicz/interviewtask/InterviewtaskApplication.java
@@ -0,0 +1,12 @@
+package com.murbanowicz.interviewtask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterviewtaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InterviewtaskApplication.class, args);
+    }
+}
---
+++ src/test/java/com/murbanowicz/interviewtask/InterviewtaskApplicationTests.java
@@ -0,0 +1,12 @@
+package com.murbanowicz.interviewtask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterviewtaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
